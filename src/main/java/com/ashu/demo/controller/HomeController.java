91452ed8bc3b4b3ccb87861205393e6eb8e40f11
jaguar2014 <emailmulat@gmail.com>
2018-02-16 10:45:55 -0500

    added startbootstrap Theme to the application

--- src/main/java/com/ashu/demo/controller/HomeController.java
+++ src/main/java/com/ashu/demo/controller/HomeController.java
@@ -218,7 +218,7 @@ public class HomeController {
 
         model.addAttribute("references", referenceRepository.findAll());
 
-        return "displayresume";
+        return "finalresume";
     }
 
     @GetMapping("/displaycontact")
--- src/main/java/com/ashu/demo/security/SecurityConfiguration.java
+++ src/main/java/com/ashu/demo/security/SecurityConfiguration.java
@@ -15,7 +15,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-     auth.inMemoryAuthentication().withUser("user").password("pass").authorities("APPLICANT")
+     auth.inMemoryAuthentication().withUser("applicant").password("pass").authorities("APPLICANT")
              .and()
              .withUser("employer").password("emppass").authorities("EMPLOYER");
     }
@@ -24,8 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/","/image/**","/css/**","/js/**","/h2-console/**").permitAll()
-                .antMatchers("/addcontact").access("hasAuthority('applicant')")
+                .antMatchers("/vendor/**","/image/**","/css/**","/js/**","/scss/**","/h2-console/**").permitAll()
+                .antMatchers("/addcontact","/addeducation","/addexperience","/addskill","/addreference").access("hasAuthority('APPLICANT')")
                 .antMatchers("/displaycompletedresume","/displaycompletedcover").access("hasAuthority('APPLICANT') or hasAuthority('EMPLOYER')")
                 .anyRequest().authenticated()
 
